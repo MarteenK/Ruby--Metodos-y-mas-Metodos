7987ea4289c57e876274209b463663948088ce49
Martin Karlsson <83670863+MarteenK@users.noreply.github.com>
2022-10-29 18:04:15 +0100

    en proceso

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # Ruby--Metodos-y-mas-Metodos
+Métodos y más métodos en ruby
+Define los métodos que se indican con las operaciones necesarias para que el resultado de las comparaciones finales driver code sean true:
+
+#sum methods
+
+#driver code
+metodo1(3) == metodo2(6)
+
+#subtraction methods
+
+#driver code
+metodo3(4, 6) == metodo4(9, 3)
+
+#combined arithmetic expression (all arithmetic operation methods)
+
+#driver code
+metodo5(3, 7, 8, 2) == metodo6(3, 8, 2, 9)
+
+#multiplication methods
+
+#driver code
+metodo7(23, 34) == metodo8(12, 2)
+
+
+#division & module methods
+
+#driver code
+metodo9(2, 4, 6) == metodo10(4, 6, 10)
+
+
+#joining strings method
+
+#driver code
+metodo11("Saludos") == "Te mando saludos Marcos"
---
+++ metodos-y-mas-metodos.rb
@@ -0,0 +1,43 @@
+# Ruby--Metodos-y-mas-Metodos
+# Métodos y más métodos en ruby
+# Define los métodos que se indican con las operaciones necesarias para que el resultado de las
+# comparaciones finales driver code sean true:
+
+#sum methods
+def metodo1(x)
+  return x if (x*3)
+end
+
+def metodo2(x)
+  return x if (x*3)
+end
+
+#driver code
+p metodo1(3) == metodo2(6)
+
+#subtraction methods
+
+#driver code
+#metodo3(4, 6) == metodo4(9, 3)
+
+#combined arithmetic expression (all arithmetic operation methods)
+
+#driver code
+#metodo5(3, 7, 8, 2) == metodo6(3, 8, 2, 9)
+
+#multiplication methods
+
+#driver code
+#metodo7(23, 34) == metodo8(12, 2)
+
+
+#division & module methods
+
+#driver code
+#metodo9(2, 4, 6) == metodo10(4, 6, 10)
+
+
+#joining strings method
+
+#driver code
+#metodo11("Saludos") == "Te mando saludos Marcos"
